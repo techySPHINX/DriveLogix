602a3ea9cc72629c16c52ad75c199d14564d81b5
techySPHINX <shimon123universe@gmail.com>
2024-11-18 00:26:28 +0530

    create: fucntionalities

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo"],
+  plugins: ["import"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
